e6d421ba94dafcb3de9b0851bd32e04af2ab5852
Khaledalnaie <abo.naie.93@gmail.com>
2020-05-19 17:31:12 +0300

    pro2

---
+++ pro2.js
@@ -0,0 +1,16 @@
+ var btn = document.createElement("BUTTON");
+  document.getElementById("a").appendChild(btn);
+   btn.innerHTML="fligth";
+   btn.setAttribute("id","fligth");
+
+  var btn = document.createElement("BUTTON");
+  document.getElementById("a").appendChild(btn);
+   btn.innerHTML="hotel";
+   btn.setAttribute("id","hotel");
+
+var btn = document.createElement("BUTTON");
+  document.getElementById("a").appendChild(btn);
+   btn.innerHTML="Tourism trips";
+   btn.setAttribute("id","Tourism-trips");
+
+
